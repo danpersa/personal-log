4e780c63477c0854ba0d2e836531a77509b6fcf8
Dan Persa <dan.persa@gmail.com>
2011-02-05 21:16:24 +0200

    done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 PersonalLog::Application.routes.draw do
-  resources :users  
+
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   root :to => 'pages#home'
   match '/signup',  :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
