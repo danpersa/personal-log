1e22513a16994b6d760d5011d075d074d8034167
Dan Persa <dan.persa@gmail.com>
2011-02-18 22:06:34 +0200

    added microposts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ PersonalLog::Application.routes.draw do
 
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
 
   root :to => 'pages#home'
   match '/signup',  :to => 'users#new'
