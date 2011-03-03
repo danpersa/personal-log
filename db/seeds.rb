4216300960170968e6b1d6d4c5e28ad01ed5b810
Dan Persa <dan.persa@gmail.com>
2011-03-03 17:03:13 +0200

    added reference data for privacy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+Privacy.create!(:name => "public")
+Privacy.create!(:name => "private")
