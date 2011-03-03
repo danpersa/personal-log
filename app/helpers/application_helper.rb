098102de6160fb5c39d592366d41168bb907f02c
Dan Persa <dan.persa@gmail.com>
2011-03-03 17:41:54 +0200

    added some application description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,6 @@ module ApplicationHelper
   end
 
   def logo
-    image_tag("logo.png", :alt => "Sample App", :class => "round")
+    image_tag("remember.png", :alt => "Sample App", :class => "round")
   end
 end
