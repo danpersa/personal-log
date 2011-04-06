976e819e1660631894cc4c3b718948c9f86f45a0
Dan Persa <dan.persa@gmail.com>
2011-04-06 17:56:02 +0300

    fixed validation failure on home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,6 +10,7 @@ class MicropostsController < ApplicationController
       redirect_to root_path
     else
       @feed_items = []
+      @user = current_user
       render 'pages/home'
     end
   end
