521d330c535ef381538db4fac156169df7f0dfc1
Dan Persa <dan.persa@gmail.com>
2011-04-06 16:18:55 +0300

    remove post button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  $('div.hover').hover(
+    function() { $(this).addClass('ui-state-hover'); }, 
+    function() { $(this).removeClass('ui-state-hover'); }
+  );
+  
+  $('tr.show-close-button').hover(
+    function() { $(this).find("div.icon-button").removeClass('invisible'); },
+    function() { $(this).find("div.icon-button").addClass('invisible'); }
+  );
+});
