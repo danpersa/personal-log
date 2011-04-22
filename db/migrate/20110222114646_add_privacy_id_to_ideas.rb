7edd29a413141c8ddf9fbb7b3ddcd83515a01134
Dan Persa <dan.persa@gmail.com>
2011-04-22 17:39:42 +0300

    removed privacy from idea

--- db/migrate/20110222114646_add_privacy_id_to_ideas.rb
+++
@@ -1,9 +0,0 @@
-class AddPrivacyIdToIdeas < ActiveRecord::Migration
-  def self.up
-    add_column :ideas, :privacy_id, :integer
-  end
-
-  def self.down
-    remove_column :ideas, :privacy_id
-  end
-end
