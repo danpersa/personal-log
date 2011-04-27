a29a4785ba4ea861a4c4dbb593a10406b11ff8f3
Dan Persa <dan.persa@gmail.com>
2011-04-27 17:53:04 +0300

    time format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,2 @@
-Date::DATE_FORMATS[:default] = "%d/%m/%Y"
-Time::DATE_FORMATS[:default] = "%d/%m/%Y"
+Date::DATE_FORMATS[:default] = lambda { |date| I18n.l(date) }
+Time::DATE_FORMATS[:default] = lambda { |date| I18n.l(date) }
