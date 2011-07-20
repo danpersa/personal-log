6bc9a797ad7db16fe899453448d9ebc7a114c37b
Dan Persa <dan.persa@gmail.com>
2011-07-20 14:35:57 +0300

    added TODO

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,6 +17,7 @@ class PagesController < ApplicationController
       @newest_public_or_own_reminder_for_idea = {}
       @feed_items.each do |idea|
         @users_with_public_or_own_reminders_for_idea[idea] = User.users_with_public_or_own_reminders_for_idea(idea, current_user).includes(:profile).limit(2).all
+        # TODO user count instead of all.size
         @users_with_public_or_own_reminders_for_idea_count[idea] = User.users_with_public_or_own_reminders_for_idea(idea, current_user).all.size
         
         logger.info "@users_with_public_or_own_reminders_for_idea_count[idea] "
