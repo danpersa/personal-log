8369dec4d3484b340f2b3b0bf4dc8795b2c5b4c5
Dan Persa <dan.persa@gmail.com>
2011-10-03 22:50:24 +0300

    tests for good and done ideas controllers

--- spec/controllers/done_ideas_controller_spec.rb
+++ spec/controllers/done_ideas_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe DoneIdeasController do
+  render_views
+  
+  describe "POST 'create'" do
 
+    before(:each) do
+      @user = Factory.create(:user)
+      @idea = Factory.create(:idea, :user => @user)
+      test_sign_in(@user)
+    end
+    
+    describe "success" do
+       
+      it "should create a done idea" do
+        lambda do
+          post :create, :id => @idea.id
+        end.should change(DoneIdea, :count).by(1)
+      end
+    end
+  end
+  
+  
+  describe "DELETE 'destroy'" do
+
+    before(:each) do
+      @user = Factory.create(:user)
+      @idea = Factory.create(:idea, :user => @user)
+      @done_idea = Factory.create(:done_idea, :idea => @idea, :user => @user)
+      test_sign_in(@user)
+    end
+    
+    describe "success" do
+       
+      it "should destroy a done idea" do
+        lambda do
+          delete :destroy, :id => @idea.id
+        end.should change(DoneIdea, :count).by(-1)
+      end
+    end
+  end
 end
--- spec/controllers/good_ideas_controller_spec.rb
+++ spec/controllers/good_ideas_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe GoodIdeasController do
+  render_views
+  
+  describe "POST 'create'" do
 
+    before(:each) do
+      @user = Factory.create(:user)
+      @idea = Factory.create(:idea, :user => @user)
+      test_sign_in(@user)
+    end
+    
+    describe "success" do
+       
+      it "should create a good idea" do
+        lambda do
+          post :create, :id => @idea.id
+        end.should change(GoodIdea, :count).by(1)
+      end
+    end
+  end
+  
+  
+  describe "DELETE 'destroy'" do
+
+    before(:each) do
+      @user = Factory.create(:user)
+      @idea = Factory.create(:idea, :user => @user)
+      @good_idea = Factory.create(:good_idea, :idea => @idea, :user => @user)
+      test_sign_in(@user)
+    end
+    
+    describe "success" do
+       
+      it "should destroy a good idea" do
+        lambda do
+          delete :destroy, :id => @idea.id
+        end.should change(GoodIdea, :count).by(-1)
+      end
+    end
+  end
 end
